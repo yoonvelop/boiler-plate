9b8f4bc2f2140a23d8234d8af6c10f10f5f661df
yoonvelop <yoonvelop@gmail.com>
2020-04-18 22:01:11 +0900

    feat: logout

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt')
 const saltRounds = 10 // saltRound를 10자리로 생성
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name : {
