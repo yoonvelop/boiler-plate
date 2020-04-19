c59e1aea5597e81458929f76a8457917a644a9aa
yoonvelop <yoonvelop@gmail.com>
2020-04-19 17:00:13 +0900

    feat: router

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import LandingPage from './components/views/LandingPage/LandingPage'
+import LoginPage from './components/views/LoginPage/LoginPage'
+import RegisterPage from './components/views/RegisterPage/RegisterPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+    
+      <Switch>
+        <Route exact path="/" component={LandingPage}/>
+        <Route exact path="/login" component={LoginPage}/>
+        <Route exact path="/register" component={RegisterPage}/>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
